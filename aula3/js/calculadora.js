c22622573f6201f0110d1749da6e43a0cefaa360
Gabriel-Teodoro <gabrieltodoro1@hotmail.com>
2022-10-25 22:37:59 -0300

    Aula 3/4/5

---
+++ aula3/js/calculadora.js
@@ -0,0 +1,6 @@
+import somar from "./modules/somar.js";
+import subtrair from "./modules/subtrair.js";
+import multiplicar from "./modules/multiplicar.js";
+import dividir from "./modules/dividir.js";
+
+console.log(somar(5,6));
---
+++ aula4/Aula 4 - Introdução ao DOM/app.js
@@ -0,0 +1,5 @@
+
+
+let teste = document.querySelectorAll('section article h2')[0];
+
+console.log(teste)
